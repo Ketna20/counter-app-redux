3d45a030a0c1a6a8789aa7f7a29ad37cfcbec879
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2020-06-21 22:11:30 -0700

    counter app using react and redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+//import counterReducer from './store/reducers/counter';
+//import resultReducer from './store/reducers/result';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// const rootReducer = combineReducers ({
+//   ctr: counterReducer,
+//   res: resultReducer
+// });
+
+// const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode> ,
+  // </Provider>,
   document.getElementById('root')
 );
 
