40a783d6f4b6585b8308878af810c734e750274d
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2020-07-21 18:08:46 -0700

    redux implementation added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-//import counterReducer from './store/reducers/counter';
-//import resultReducer from './store/reducers/result';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducer';
 
-// const rootReducer = combineReducers ({
-//   ctr: counterReducer,
-//   res: resultReducer
-// });
 
- const store = createStore(reducer);
+const rootReducer = combineReducers ({
+  ctr: counterReducer,
+  res: resultReducer
+});
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  }
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
-  // <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode> ,
-  // </Provider>,
+  </Provider>,
   document.getElementById('root')
 );
 
